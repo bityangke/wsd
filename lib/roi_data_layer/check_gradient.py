34df54bad7ed5529f471bf969b6b0b539a67c241
marco pedersoli <mpederso@artemis.inrialpes.fr>
2016-02-16 14:41:57 +0100

    added two missing files

---
+++ lib/roi_data_layer/check_gradient.py
@@ -0,0 +1,144 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""The data layer used during training to train a Fast R-CNN network.
+
+RoIDataLayer implements a Caffe Python layer.
+"""
+
+import numpy as np
+import caffe
+from fast_rcnn.config import cfg
+#from scipy.optimize import check_grad
+
+def approx_fprime(px,f,epsilon=1e-4):
+    xp=px.copy()
+    y=f(px) 
+    grad=np.zeros(list(px.shape),px.dtype)
+    for x in xrange(px.shape[0]):
+        #for y in xrange(px.shape[1]):
+        #    for z in xrange(px.shape[2]):
+        #        for h in xrange(px.shape[3]):
+        xp[x,:]=xp[x,:]+epsilon
+        fp=f(xp)
+        xp[x,:]=xp[x,:]-2*epsilon
+        fn=f(xp)
+        xp[x,:]=xp[x,:]+epsilon
+        grad[x,:]=(fp[0,:]-fn[0,:])/(2*epsilon)
+        #print grad[x,0]
+        #raw_input()
+        #print grad[x,0]
+    return grad
+
+def check_grad(f,g,x,epsilon=1e-4):
+    return np.sum((approx_fprime(x,f,epsilon)-g(x))**2)
+
+def f(x):
+    return x.sum(0,keepdims=True)
+    
+def g(x):
+    return np.ones(x.shape,dtype=x.dtype)
+
+class MySumLayer(caffe.Layer):
+
+    def setup(self, bottom, top):
+        # check input pair
+        if len(bottom) != 1:
+            raise Exception("Only one input needed.")
+
+    def reshape(self, bottom, top):
+        bottom[0].reshape(bottom[0].num, bottom[0].channels,bottom[0].height, bottom[0].width)
+        top[0].reshape(1, bottom[0].channels,bottom[0].height, bottom[0].width)
+        top[0].diff.reshape(1, bottom[0].channels,bottom[0].height, bottom[0].width)
+
+    def forward(self, bottom, top):
+        top[0].data[...]=f(bottom[0].data)
+
+    def backward(self, top, propagate_down, bottom):
+        if propagate_down[0]:
+            #bottom[0].diff[...] = top[0].diff.squeeze()*self.weights
+            bottom[0].diff[...] = top[0].diff*g(bottom[0].data)
+            
+        if cfg.TRAIN.CHECK_GRAD:
+            x = bottom[0].data
+            err=check_grad(f,g,x,1e-4)
+            print "Testing Gradient Sum",err
+            if err>1e-3:
+                dsfsd
+                print "Error in the gradient!"
+                
+
+
+def betaweights2(x,beta,axis=-1):
+    e_x = beta**(x - np.max(x,axis=axis,keepdims=True))
+    #print e_x
+    ssum=e_x.sum(axis=axis,keepdims=True)
+    out = e_x / ssum
+    #entr = np.sum(e_x*x,axis=axis,keepdims=True)
+    return out
+
+def approx_fprime_soft(px,f,epsilon=1e-4):
+    xp=px.copy()
+    y=f(px) 
+    grad=np.zeros(list(px.shape),px.dtype)
+    for x in xrange(px.shape[0]):
+        #for y in xrange(px.shape[1]):
+        #    for z in xrange(px.shape[2]):
+        #        for h in xrange(px.shape[3]):
+        xp[x,:]+=epsilon
+        fp=f(xp)
+        xp[x,:]-=2*epsilon
+        fn=f(xp)
+        xp[x,:]+=epsilon
+        grad[x,:]=(fp[0,:]-fn[0,:])/(2*epsilon)
+        #print grad[x,0]
+    return grad
+
+#def check_grad_soft(f_soft,g_soft,x,epsilon=1e-4):
+#    return np.sum((approx_fprime_soft(x,f_soft,epsilon)-g_soft(x))**2)
+
+def f_soft(x):
+    return betaweights2(x,cfg.TRAIN.BETA,axis=0)
+    
+def g_soft(x):
+    p = betaweights2(x,cfg.TRAIN.BETA,axis=0)
+    return np.log(cfg.TRAIN.BETA)* p * (1-(p).sum(0,keepdims=True))
+            
+class BetaSoftMaxLayer(caffe.Layer):
+
+    def setup(self, bottom, top):
+        if cfg.TRAIN.BETA==-1:
+            layer_params = yaml.load(self.param_str_)
+            self.beta = layer_params['beta']
+        else:
+            self.beta = cfg.TRAIN.BETA
+        print "Beta",self.beta 
+        self.it=0
+        if len(bottom) != 1:
+            raise Exception("Only one input needed.")
+
+    def reshape(self, bottom, top):
+        top[0].reshape(bottom[0].num, bottom[0].channels)
+
+    def forward(self, bottom, top):
+        self.it+=1
+        #p = betaweights2(bottom[0].data,self.beta,axis=0)
+        top[0].data[...] = f_soft(bottom[0].data)#p
+
+    def backward(self, top, propagate_down, bottom):
+        #bottom[0].diff[...] = np.log(self.beta)*top[0].data * (top[0].diff-(top[0].diff*top[0].data).sum(0,keepdims=True))#/bottom[0].num
+        bottom[0].diff[...] = top[0].diff*g_soft(bottom[0].data)
+        #print 'Error', np.sum((bottom[0].diff-pr2)**2)
+        if cfg.TRAIN.CHECK_GRAD:
+            x = bottom[0].data
+            err=check_grad(f_soft,g_soft,x,1e-4)
+            print "Testing Gradient Softmax",err
+            if err>1e-3:
+                print "Error in the softmax gradient!"
+                sfdfs
+
+
---
+++ lib/roi_data_layer/sum.py
@@ -0,0 +1,150 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""The data layer used during training to train a Fast R-CNN network.
+
+RoIDataLayer implements a Caffe Python layer.
+"""
+
+#bottom[0]=(128,21,1,1) #classification scores
+#bottom[1]=(21) #image labels
+#for the moment I assume an image for each batch, but not a good assumption!!! 
+
+import caffe
+import numpy as np
+
+def softmax(x,axis=-1):
+    #e_x = np.exp(x - np.max(x,axis=axis))
+    #out = e_x / e_x.sum(axis=axis)
+    mmax = np.max(x,axis=axis,keepdims=True)
+    #print mmax.shape,(x-mmax).shape,(np.log(np.sum(np.exp(x - mmax),axis=axis))).shape
+    out = mmax.squeeze() + np.log(np.sum(np.exp(x - mmax),axis=axis))
+    return out
+
+def weights(x,axis=-1):
+    e_x = np.exp(x - np.max(x,axis=axis,keepdims=True))
+    #print e_x
+    out = e_x / e_x.sum(axis=axis,keepdims=True)
+    return out
+
+class MyMeanLayer(caffe.Layer):
+
+    def setup(self, bottom, top):
+        # check input pair
+        if len(bottom) != 1:
+            raise Exception("Only one input needed.")
+
+    def reshape(self, bottom, top):
+        top[0].reshape(1, bottom[0].channels,
+                bottom[0].height, bottom[0].width)
+        #print top[0].data.shape
+        #sfsfd
+        #self.diff = np.zeros_like(bottom[0].data, dtype=np.float32)
+        # check input dimensions match
+        #if bottom[0].data.shape[1] != bottom[1].data.shape[0]:
+        #    raise Exception("There should be a 0-1 label per class, per image.")
+        # difference is shape of inputs
+        #self.diff = np.zeros(bottom[0].data.shape(1), dtype=np.float32)
+        # loss output is scalar
+        #top[0].reshape(bottom[0].data.shape(1))
+
+    def forward(self, bottom, top):
+        #self.diff[...] = bottom[0].data - bottom[1].data
+        #top[0].data[...] = np.sum(self.diff**2) / bottom[0].num / 2.
+      
+        #softmax in dimension 0 
+        if 1: #mean
+            max_cls = np.mean(bottom[0].data,axis=0)
+            top[0].data[0,:,0,0] = max_cls
+            aux = np.ones(bottom[0].data.shape,dtype=bottom[0].data.dtype)
+            self.weights = aux/bottom[0].data.shape[0]#weights(bottom[0].data,axis=0)
+        else: #softmax
+            max_cls = softmax(bottom[0].data,axis=0)
+            top[0].data[0,:,0,0] = max_cls
+            self.weights = weights(bottom[0].data,axis=0)
+        if 0:
+            import pylab
+            pylab.figure(1)
+            pylab.plot(max_cls)
+            pylab.figure(2)
+            pylab.imshow(self.weights)
+            pylab.show()
+            sdfsd
+            raw_input()
+        #print top[0].data.shape
+        #fsf
+        #cross entropy in dimension 0
+        
+
+    def backward(self, top, propagate_down, bottom):
+        if propagate_down[0]:
+            bottom[0].diff[...] = top[0].diff.squeeze()*self.weights
+            if 0:
+                print "delta",top[0].diff.squeeze()
+                print "weights",self.weights
+                print "bottom",bottom[0].diff.squeeze()
+                raw_input()
+        #print top[0].diff.squeeze()
+        #raw_input()
+        #print self.weights
+        #print "Bottom",bottom[0].diff.sum(0).squeeze()
+        
+class MySumLayer(caffe.Layer):
+
+    def setup(self, bottom, top):
+        # check input pair
+        if len(bottom) != 1:
+            raise Exception("Only one input needed.")
+
+    def reshape(self, bottom, top):
+        top[0].reshape(1, bottom[0].channels,
+                bottom[0].height, bottom[0].width)
+        #print top[0].data.shape
+        #sfsfd
+        #self.diff = np.zeros_like(bottom[0].data, dtype=np.float32)
+        # check input dimensions match
+        #if bottom[0].data.shape[1] != bottom[1].data.shape[0]:
+        #    raise Exception("There should be a 0-1 label per class, per image.")
+        # difference is shape of inputs
+        #self.diff = np.zeros(bottom[0].data.shape(1), dtype=np.float32)
+        # loss output is scalar
+        #top[0].reshape(bottom[0].data.shape(1))
+
+    def forward(self, bottom, top):
+        #self.diff[...] = bottom[0].data - bottom[1].data
+        #top[0].data[...] = np.sum(self.diff**2) / bottom[0].num / 2.
+      
+        #softmax in dimension 0 
+        if 1: #mean
+            max_cls = np.sum(bottom[0].data,axis=0)
+            top[0].data[0,:,0,0] = max_cls
+            aux = np.ones(bottom[0].data.shape,dtype=bottom[0].data.dtype)
+            self.weights = aux#/bottom[0].data.shape[0]#weights(bottom[0].data,axis=0)
+        if 0:
+            import pylab
+            pylab.figure(1)
+            pylab.plot(max_cls)
+            pylab.figure(2)
+            pylab.imshow(self.weights)
+            pylab.show()
+            sdfsd
+            raw_input()
+        #print top[0].data.shape
+        #fsf
+        #cross entropy in dimension 0
+        
+
+    def backward(self, top, propagate_down, bottom):
+        if propagate_down[0]:
+            bottom[0].diff[...] = top[0].diff.squeeze()*self.weights
+            if 0:
+                print "delta",top[0].diff.squeeze()
+                print "weights",self.weights
+                print "bottom",bottom[0].diff.squeeze()
+                raw_input()
+          
+
